c5fa16bafb6eabd28047059940a5c50de3fc01cc
Fouad CHAMOUN <fchamoun@scaleway.com>
2026-05-07 22:27:01 +0200

    feat(talos): add extra host entry for hass.home

--- k8s/talos/talos.tf
+++ k8s/talos/talos.tf
@@ -139,6 +139,20 @@ ephemeral "talos_machine_configuration" "controlplane" {
       ip_cidr = "${each.value.ip}/24"
       ip_gateway = "192.168.200.1"
     }),
+    yamlencode({
+      machine = {
+        network = {
+          extraHostEntries = [
+            {
+              ip = "192.168.200.13"
+              aliases = [
+                "hass.home"
+              ]
+            }
+          ]
+        }
+      }
+    })
   ]
 }
 
