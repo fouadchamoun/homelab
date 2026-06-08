944444bd4f24294e3a307710c6934598a223e78d
Fouad CHAMOUN <fchamoun@scaleway.com>
2026-06-08 20:55:23 +0200

    refactor(argocd): move bootstrap project/app back to terraform
    to avoid circular dependency

--- k8s/terraform/argocd.tf
+++ k8s/terraform/argocd.tf
@@ -9,7 +9,7 @@ resource "kubernetes_namespace_v1" "argocd" {
 
 resource "kubernetes_secret_v1" "argocd_secret" {
   metadata {
-    name = "argocd-secret"
+    name      = "argocd-secret"
     namespace = kubernetes_namespace_v1.argocd.metadata[0].name
   }
 
@@ -17,13 +17,108 @@ resource "kubernetes_secret_v1" "argocd_secret" {
 
   data_wo_revision = 2
   data_wo = {
-    "admin.password" = bcrypt(ephemeral.sops_file.secrets.data["argocd.admin-password"])
-    "admin.passwordMtime" = timestamp()
-    "server.secretkey" = ephemeral.sops_file.secrets.data["argocd.server-secretkey"]
+    "admin.password"        = bcrypt(ephemeral.sops_file.secrets.data["argocd.admin-password"])
+    "admin.passwordMtime"   = timestamp()
+    "server.secretkey"      = ephemeral.sops_file.secrets.data["argocd.server-secretkey"]
     "webhook.github.secret" = ephemeral.sops_file.secrets.data["github-webhooks.secret"]
   }
 
   lifecycle {
-    ignore_changes = [ metadata ]
+    ignore_changes = [metadata]
+  }
+}
+
+resource "argocd_project" "cluster_bootstrap" {
+  depends_on = [kubernetes_secret_v1.scw_sm_secret]
+
+  metadata {
+    name      = "cluster-bootstrap"
+    namespace = kubernetes_namespace_v1.argocd.metadata[0].name
+  }
+
+  spec {
+    description = "Cluster Bootstrap"
+
+    source_repos = [
+      "https://github.com/fouadchamoun/homelab.git",
+      "https://github.com/argoproj/argo-cd",
+      "https://argoproj.github.io/argo-helm",
+      "ghcr.io/piraeusdatastore/piraeus-operator",
+      "ghcr.io/piraeusdatastore/helm-charts",
+      "quay.io/jetstack/charts",
+      "https://charts.external-secrets.io",
+      "https://traefik.github.io/charts",
+      "https://charts.signoz.io",
+      "ghcr.io/kite-org/charts"
+    ]
+
+    cluster_resource_whitelist {
+      group = "*"
+      kind  = "*"
+    }
+
+    destination {
+      server    = "https://kubernetes.default.svc"
+      namespace = "argocd"
+    }
+    destination {
+      server    = "https://kubernetes.default.svc"
+      namespace = "kube-system"
+    }
+    destination {
+      server    = "https://kubernetes.default.svc"
+      namespace = "piraeus-datastore"
+    }
+    destination {
+      server    = "https://kubernetes.default.svc"
+      namespace = "external-secrets"
+    }
+    destination {
+      server    = "https://kubernetes.default.svc"
+      namespace = "cert-manager"
+    }
+    destination {
+      server    = "https://kubernetes.default.svc"
+      namespace = "traefik"
+    }
+    destination {
+      server    = "https://kubernetes.default.svc"
+      namespace = "signoz"
+    }
+    destination {
+      server    = "https://kubernetes.default.svc"
+      namespace = "kite-system"
+    }
+  }
+}
+
+# Application - Cluster Bootstrap
+resource "argocd_application" "cluster_bootstrap" {
+  metadata {
+    name      = "cluster-bootstrap"
+    namespace = kubernetes_namespace_v1.argocd.metadata[0].name
+  }
+
+  spec {
+    project = argocd_project.cluster_bootstrap.metadata[0].name
+
+    source {
+      repo_url        = "https://github.com/fouadchamoun/homelab.git"
+      target_revision = "main"
+      path            = "k8s/bootstrap"
+    }
+
+    destination {
+      server    = "https://kubernetes.default.svc"
+      namespace = "argocd"
+    }
+
+    # sync_policy {
+    #   automated {
+    #     prune       = false
+    #     allow_empty = false
+    #     self_heal   = true
+    #   }
+    # }
   }
 }
