da5a997f21a4e94e469468f74f42278a20330f66
Fouad Chamoun <cesure_potee_0u@icloud.com>
2026-06-30 12:24:17 +0200

    feat(pve): add ACME certs (#314)

--- pve/terraform/acme-cloudflare.tf
+++ pve/terraform/acme-cloudflare.tf
@@ -1,12 +1,39 @@
 resource "proxmox_acme_dns_plugin" "cloudflare" {
-  api    = "cf"
-  plugin = "cloudflare"
+  api              = "cf"
+  plugin           = "cloudflare"
   validation_delay = 0
   data = {
     CF_Account_ID = data.sops_file.secrets.data["cloudflare.account_id"]
-    CF_Email = data.sops_file.secrets.data["cloudflare.email"]
-    CF_Key = data.sops_file.secrets.data["cloudflare.key"]
-    CF_Token = data.sops_file.secrets.data["cloudflare.token"]
-    CF_Zone_ID = data.sops_file.secrets.data["cloudflare.zone_id"]
+    CF_Email      = data.sops_file.secrets.data["cloudflare.email"]
+    CF_Key        = data.sops_file.secrets.data["cloudflare.key"]
+    CF_Token      = data.sops_file.secrets.data["cloudflare.token"]
+    CF_Zone_ID    = data.sops_file.secrets.data["cloudflare.zone_id"]
+  }
+}
+
+resource "proxmox_acme_account" "default" {
+  provider = proxmox.root
+
+  name      = "default"
+  contact   = "mail@fouad.dev"
+  directory = "https://acme-v02.api.letsencrypt.org/directory"
+  tos       = "https://letsencrypt.org/documents/LE-SA-v1.4-April-3-2024.pdf"
+}
+
+resource "proxmox_acme_certificate" "webui_certificate" {
+  for_each = toset(["pve-0", "pve-1", "pve-2"])
+
+  node_name = each.value
+  account   = proxmox_acme_account.default.name
+
+  domains = [
+    {
+      domain = "${each.value}.homelab.fouad.dev"
+      plugin = proxmox_acme_dns_plugin.cloudflare.plugin
+    }
+  ]
+
+  lifecycle {
+    ignore_changes = []
   }
 }
--- pve/terraform/main.tf
+++ pve/terraform/main.tf
@@ -13,15 +13,15 @@ terraform {
       version = "0.112.0"
     }
     proxmox = {
-      source = "bpg/proxmox"
+      source  = "bpg/proxmox"
       version = "0.110.0"
     }
     sops = {
-      source = "carlpett/sops"
+      source  = "carlpett/sops"
       version = "1.4.1"
     }
     http = {
-      source = "hashicorp/http"
+      source  = "hashicorp/http"
       version = "3.6.0"
     }
   }
@@ -38,6 +38,14 @@ data "sops_file" "secrets" {
 provider "proxmox" {
   endpoint = "https://pve-0.homelab.fouad.dev:8006"
 
-  # Choose one authentication method:
-  api_token = data.sops_file.secrets.data["pve.cluster_api_token"]
+  api_token = data.sops_file.secrets.data["pve.terraform_api_token"]
+}
+
+provider "proxmox" {
+  alias = "root"
+
+  endpoint = "https://pve-0.homelab.fouad.dev:8006"
+
+  username = "root@pam"
+  password = data.sops_file.secrets.data["pve.root_password"]
 }
