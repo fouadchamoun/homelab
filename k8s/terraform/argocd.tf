d46fc0e2eb14214f5589edae2ee794229efb7184
Fouad CHAMOUN <fchamoun@scaleway.com>
2026-09-15 21:48:08 +0200

    feat(monitoring): add node-problem-detector

--- k8s/terraform/argocd.tf
+++ k8s/terraform/argocd.tf
@@ -49,7 +49,8 @@ resource "argocd_project" "cluster_bootstrap" {
       "https://charts.external-secrets.io",
       "https://traefik.github.io/charts",
       "ghcr.io/kite-org/charts",
-      "ghcr.io/prometheus-community/charts"
+      "ghcr.io/prometheus-community/charts",
+      "ghcr.io/deliveryhero/helm-charts"
     ]
 
     cluster_resource_whitelist {
