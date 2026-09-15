d2497d74ff9d600c25e2399038721df228e8ce8b
Fouad CHAMOUN <fchamoun@scaleway.com>
2026-09-15 22:59:26 +0200

    feat(monitoring): add descheduler

--- k8s/terraform/argocd.tf
+++ k8s/terraform/argocd.tf
@@ -50,7 +50,8 @@ resource "argocd_project" "cluster_bootstrap" {
       "https://traefik.github.io/charts",
       "ghcr.io/kite-org/charts",
       "ghcr.io/prometheus-community/charts",
-      "ghcr.io/deliveryhero/helm-charts"
+      "ghcr.io/deliveryhero/helm-charts",
+      "https://kubernetes-sigs.github.io/descheduler"
     ]
 
     cluster_resource_whitelist {
